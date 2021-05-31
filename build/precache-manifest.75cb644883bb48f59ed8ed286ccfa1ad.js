8bdbfb11ada552ba5967cb556b24cfb4e7b3b887
unknown <sharmachandan487@gmail.com>
2021-05-31 17:07:15 +0530

    first value

---
+++ build/precache-manifest.75cb644883bb48f59ed8ed286ccfa1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9cb6aa68ceb31f2427c2459f3d3316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2fd980006230d61acc4",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "7ccb9089acef15d5ec5f",
+    "url": "/static/js/2.58a8787f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.58a8787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2fd980006230d61acc4",
+    "url": "/static/js/main.a9cc832f.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  }
+]);
